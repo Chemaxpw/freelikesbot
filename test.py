8b3895523c0dab606f9fad23e754936576cbb591
Chemaxpw <46086545+Chemaxpw@users.noreply.github.com>
2018-12-28 23:28:36 +0500

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
+def get_element(selector):
+	for i in range(4):
+		sleep(2)
+		try:
+			element = driver.find_element_by_css_selector(selector)
+		except:
+			pass
+		else:
+			return element
+return None
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument("log-level=3") 
 driver = webdriver.Chrome("./chromedriver",chrome_options=options)
-driver.get("https://pikabu.ru/")
+driver.get("http://freelikes.online/")
+get_element(".socico.ytico").click()
+get_element("#identifierId").send_keys("markata89")
+get_element(".CwaK9").click()
+get_element(".whsOnd.zHQkBf").send_keys("rvtrbtrb564565")
+get_element(".CwaK9").click()
+sleep(3)
+driver.get("http://freelikes.online/earn/youtube/ytlike")
 print(driver.title)
-print(driver.find_element_by_css_selector(".story__title-link").text)
+print(driver.find_element_by_css_selector("body").get_attribute("innerHTML"))
