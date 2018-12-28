ebed0ebb4c7ded72557595a196acf3bf0bb19f30
Chemaxpw <46086545+Chemaxpw@users.noreply.github.com>
2018-12-28 23:04:04 +0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument("log-level=3") 
+driver = webdriver.Chrome(chrome_options=options)
+driver.get("https://pikabu.ru/")
+print(driver.title)
+print(driver.find_element_by_css_selector("body").get_attribute('innerHTML'))
