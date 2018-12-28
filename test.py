bda75dd9e9f30a82fb366e4d4e578bf23acb9245
Chemaxpw <46086545+Chemaxpw@users.noreply.github.com>
2018-12-28 23:06:21 +0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from time import sleep
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument("log-level=3") 
-driver = webdriver.Chrome(chrome_options=options)
+driver = webdriver.Chrome("./chromedriver",chrome_options=options)
 driver.get("https://pikabu.ru/")
 print(driver.title)
 print(driver.find_element_by_css_selector("body").get_attribute('innerHTML'))
