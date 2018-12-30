b157dfdb793252bfd3f8972757ec2259839fb502
chemaxpw <chemaxpw@mail.ru>
2018-12-31 00:43:27 +0500

    aa

--- test.py
+++ test.py
@@ -25,7 +25,7 @@ get_element(".whsOnd.zHQkBf").send_keys("dhfd7fhd7f8")
 get_element(".CwaK9").click()
 sleep(3)
 get_element("#profileIdentifier").click()
-sleep()
+sleep(2)
 try:
 	balance = get_element("font#points2").text
 except:
