ffa168faeddee1222babe370cd41b5c054baea86
Chemaxpw <46086545+Chemaxpw@users.noreply.github.com>
2018-12-30 17:58:55 +0500

    Update test.py

--- test.py
+++ test.py
@@ -5,18 +5,18 @@ from time import sleep
 def get_element(selector):
 	for i in range(4):
 		sleep(2)
-	try:
-		element = driver.find_element_by_css_selector(selector)
-	except:
-		pass
-	else:
-		return element
+		try:
+			element = driver.find_element_by_css_selector(selector)
+		except:
+			pass
+		else:
+			return element
 	return None
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument("log-level=3") 
-driver = webdriver.Chrome("./chromedriver",chrome_options=options)
+driver = webdriver.Chrome(chrome_options=options)
 driver.get("http://freelikes.online/")
 get_element(".socico.ytico").click()
 get_element("#identifierId").send_keys("markata89")
@@ -26,4 +26,9 @@ get_element(".CwaK9").click()
 sleep(3)
 driver.get("http://freelikes.online/earn/youtube/ytlike")
 print(driver.title)
-print(driver.find_element_by_css_selector("body").get_attribute("innerHTML"))
+try:
+	balance = get_element("font#points2").text
+except:
+	print("Not found")
+else:
+	print(balance)
